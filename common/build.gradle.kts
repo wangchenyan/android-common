94a0cb6284d05feace78b71a670ed0d79018ba68
王晨彦 <wangchenyan.top@bytedance.com>
2023-05-16 16:09:52 +0800

    add log

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -22,14 +22,15 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.6")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("com.github.soulqw:SoulPermission:1.3.1")
     implementation("com.geyifeng.immersionbar:immersionbar:3.2.2")
     implementation("com.liulishuo.filedownloader:library:1.7.7")
     implementation("com.kingja.loadsir:loadsir:1.3.8")
     implementation("top.zibin:Luban:1.1.8")
     implementation("com.github.li-xiaojun:XPopup:2.7.9")
+    implementation("com.elvishew:xlog:1.10.1")
     api("com.github.wangchenyan.crouter:crouter-api:2.2.1")
     api("com.google.code.gson:gson:2.10.1")
     api("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
