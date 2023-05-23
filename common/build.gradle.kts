b4c06871a6893a28caaf57f6672fce9a13618b4d
王晨彦 <wangchenyan.top@bytedance.com>
2023-05-23 17:28:38 +0800

    update dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -21,19 +21,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.7")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("com.github.soulqw:SoulPermission:1.3.1")
-    implementation("com.geyifeng.immersionbar:immersionbar:3.2.2")
-    implementation("com.liulishuo.filedownloader:library:1.7.7")
-    implementation("com.kingja.loadsir:loadsir:1.3.8")
-    implementation("top.zibin:Luban:1.1.8")
-    implementation("com.github.li-xiaojun:XPopup:2.7.9")
-    implementation("com.elvishew:xlog:1.10.1")
-    api("com.github.wangchenyan.crouter:crouter-api:2.2.1")
+    api("androidx.appcompat:appcompat:1.6.1")
+    api("androidx.fragment:fragment-ktx:1.6.0")
+    api("com.google.android.material:material:1.9.0")
     api("com.google.code.gson:gson:2.10.1")
-    api("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
+    api("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
     api("com.squareup.retrofit2:retrofit:2.9.0")
     api("com.blankj:utilcodex:1.31.1")
     api("com.github.bumptech.glide:glide:4.13.2")
@@ -41,7 +33,15 @@ dependencies {
     api("io.github.scwang90:refresh-layout-kernel:2.0.5")
     api("io.github.scwang90:refresh-header-material:2.0.5")
     api("io.github.scwang90:refresh-footer-classics:2.0.5")
+    api("com.github.wangchenyan.crouter:crouter-api:2.2.1")
     api("com.github.wangchenyan:radapter3:3.1.0")
+    api("com.kingja.loadsir:loadsir:1.3.8")
+    api("com.geyifeng.immersionbar:immersionbar:3.2.2")
+    implementation("com.github.soulqw:SoulPermission:1.3.1")
+    implementation("com.liulishuo.filedownloader:library:1.7.7")
+    implementation("top.zibin:Luban:1.1.8")
+    implementation("com.github.li-xiaojun:XPopup:2.7.9")
+    implementation("com.elvishew:xlog:1.10.1")
 }
 
 afterEvaluate {
