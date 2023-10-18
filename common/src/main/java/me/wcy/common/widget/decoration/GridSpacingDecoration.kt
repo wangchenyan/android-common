6db5f80fdb9e8f3bcfd3e763bb9a711b0693c0e1
wcy <wangchenyan2015@gmail.com>
2023-10-18 19:17:19 +0800

    fix GridSpacingDecoration

--- common/src/main/java/me/wcy/common/widget/decoration/GridSpacingDecoration.kt
+++ common/src/main/java/me/wcy/common/widget/decoration/GridSpacingDecoration.kt
@@ -12,8 +12,6 @@ class GridSpacingDecoration(
     private val horizontalSpacing: Int,
     private val verticalSpacing: Int,
 ) : RecyclerView.ItemDecoration() {
-    private var spanCount: Int = 1
-    private var itemCount: Int = 0
 
     override fun getItemOffsets(
         outRect: Rect,
@@ -23,61 +21,17 @@ class GridSpacingDecoration(
     ) {
         val layoutManager = parent.layoutManager
         if (layoutManager is GridLayoutManager) {
-            spanCount = layoutManager.spanCount
-            itemCount = parent.adapter?.itemCount ?: 0
-
+            val spanCount = layoutManager.spanCount
+            parent.adapter?.itemCount ?: 0
             val position = parent.getChildAdapterPosition(view)
-            if (isSingleColumn()) {
-                outRect.left = 0
-                outRect.right = 0
-            } else if (isFirstColumn(position)) {
-                outRect.left = 0
-                outRect.right = horizontalSpacing / 2
-            } else if (isLastColumn(position)) {
-                outRect.left = horizontalSpacing / 2
-                outRect.right = 0
-            } else {
-                outRect.left = horizontalSpacing / 2
-                outRect.right = horizontalSpacing / 2
-            }
-
-            if (isSingleRow()) {
-                outRect.top = 0
-                outRect.bottom = 0
-            } else if (isFirstRow(position)) {
-                outRect.top = 0
-                outRect.bottom = verticalSpacing / 2
-            } else if (isLastRow(position)) {
-                outRect.top = verticalSpacing / 2
-                outRect.bottom = 0
-            } else {
-                outRect.top = verticalSpacing / 2
-                outRect.bottom = verticalSpacing / 2
+            val column = position % spanCount
+            // column * ((1f / spanCount) * spacing)
+            outRect.left = column * horizontalSpacing / spanCount
+            // spacing - (column + 1) * ((1f / spanCount) * spacing)
+            outRect.right = horizontalSpacing - (column + 1) * horizontalSpacing / spanCount
+            if (position >= spanCount) {
+                outRect.top = verticalSpacing
             }
         }
-    }
-
-    private fun isSingleColumn(): Boolean {
-        return spanCount == 1
-    }
-
-    private fun isFirstColumn(position: Int): Boolean {
-        return position % spanCount == 0
-    }
-
-    private fun isLastColumn(position: Int): Boolean {
-        return position % spanCount == spanCount - 1
-    }
-
-    private fun isSingleRow(): Boolean {
-        return itemCount <= spanCount
-    }
-
-    private fun isFirstRow(position: Int): Boolean {
-        return position < spanCount
-    }
-
-    private fun isLastRow(position: Int): Boolean {
-        return position / spanCount == (itemCount - 1) / spanCount
     }
 }
